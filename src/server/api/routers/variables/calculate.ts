88520b42f831c9c90a9053577d6a51632bf9aa7c
Ingmar <ingmar@lowack.com>
2023-09-13 17:52:32 +0200

    feat: derived values

--- src/server/api/routers/variables/calculate.ts
+++ src/server/api/routers/variables/calculate.ts
@@ -1,3 +1,4 @@
+import FormulaParser from "fast-formula-parser";
 import { User } from "next-auth";
 
 import { Page } from "@/data/page";
@@ -14,7 +15,7 @@ export async function calculateVariables(
 ): Promise<Variables> {
   const project = await getProject(projectId);
 
-  return {
+  const internal = {
     user: {
       ...user,
       roles: await getRolesOfUserInProject(user.id, projectId),
@@ -25,8 +26,43 @@ export async function calculateVariables(
       dashboards: page.dashboards.length,
     },
     time: new Date().toISOString(),
-    ...variables,
   };
+
+  return {
+    ...calculate({ ...internal, ...variables }),
+    ...internal,
+  };
+}
+
+function calculate(variables: Variables): Variables {
+  const newVariables: Variables = {};
+  for (const [key, value] of Object.entries(variables)) {
+    if (typeof value === "string") {
+      if (value.startsWith("=")) {
+        newVariables[key] = evaluate(value, variables);
+      }
+      newVariables[key] = value;
+    } else if (typeof value === "object" && value !== null) {
+      newVariables[key] = calculate(value as Variables);
+    }
+  }
+  return newVariables;
+}
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const parser = new FormulaParser();
+
+function evaluate(formula: string, variables: Variables): string {
+  // replace every variable with its value in the formula if the value is evaluated already
+  for (const [key, value] of Object.entries(variables)) {
+    if (typeof value === "string" && !value.includes("$")) {
+      formula = value.startsWith("=")
+        ? formula.replaceAll(key, value.replace("=", ""))
+        : formula.replaceAll(key, value);
+    }
+  }
+  if (formula.includes("$")) return formula;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  return parser.parse(formula.slice(1)) as string;
 }
 
 export const defaultVariables = {
